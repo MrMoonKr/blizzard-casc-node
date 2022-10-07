09232ce8762cc5b0de5cc146c964b85935fa320c
Mr Moon <mrmoonkr@fixgames.co.kr>
2022-10-07 10:26:03 +0900

    CASCRemote 체크 필요

--- casc-local-test.js
+++ casc-local-test.js
@@ -1,13 +1,18 @@
 
 
-const CASCLocal         = require( './blizzard-casc-local' );
+//const CASCLocal         = require( './blizzard-casc-local' );
+const CASCLocal         = require( './src/casc-source-local' );
 
-const installPath       = 'H:/myGames/World of Warcraft';
+const installPath       = 'F:/myGames/World of Warcraft';
 
 const openInstall = async ( intallPaht ) => {
 
-    const casc          = new CASCLocal( installPath );
+    //const casc          = new CASCLocal( installPath );
+    //await casc.init();
+
+    let casc            = new CASCLocal( installPath ) ;
     await casc.init();
+    await casc.load( 0 );
 }
 
 openInstall( installPath );
